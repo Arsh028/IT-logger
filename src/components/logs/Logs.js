11b7b73778581d5f5ea61e16d1c08a06d6055df4
Arsh Radhanpura <arshradhanpura28@gmail.com>
2020-10-22 22:52:30 +0530

    initial commit

---
+++ src/components/logs/Logs.js
@@ -0,0 +1,24 @@
+import React,{useState,useEffect} from 'react'
+
+
+export const Logs = () => {
+    const [logs, setlogs] = useState([]);
+    const [loading, setloading] = useState(false);
+
+    //componentDidMount
+    useEffect(() => {
+      getlogs();  
+    },[])
+    const getlogs = async() =>{
+        setloading(true);
+        const res = fetch('/logs');
+        const data = (await res).json();
+        setlogs(data);
+        setloading(false);
+    }
+    return (
+        <div>
+            
+        </div>
+    )
+}
